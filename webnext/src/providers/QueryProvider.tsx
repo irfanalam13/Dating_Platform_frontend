191669d92d348f67055fc60df51024fdf71d9b66
irfanalam13 <irfanalam9682@gmail.com>
2026-05-11 15:41:19 +0545

    update

--- webnext/src/providers/QueryProvider.tsx
+++ webnext/src/providers/QueryProvider.tsx
@@ -1,23 +1,3 @@
-// // src/shared/providers/QueryProvider.tsx
-// "use client";
-
-// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// import { useState, ReactNode } from "react";
-
-// export default function QueryProvider({ children }: { children: ReactNode }) {
-//   // We use useState to ensure the client is created only once per session
-//   const [queryClient] = useState(() => new QueryClient());
-
-//   return (
-//     <QueryClientProvider client={queryClient}>
-//       {children}
-//     </QueryClientProvider>
-//   );
-// }
-
-
-
-// src/providers/QueryProvider.tsx
 "use client";
 
 import { QueryClientProvider } from "@tanstack/react-query";
